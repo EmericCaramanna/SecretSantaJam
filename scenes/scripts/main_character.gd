c50cd6dce4824746f1a3d94e455fa9aab587d9bd
Emeric <emeric.caramanna@epitech.eu>
2023-12-10 12:55:33 +0100

    weapon attack animation

--- scenes/scripts/main_character.gd
+++ scenes/scripts/main_character.gd
@@ -4,8 +4,11 @@ const SPEED = 100.0
 const WEAPON_RADIUS = 2
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var animations = $animations
+@onready var animation_player = $AnimationPlayer
 @onready var weapon = $weapon
+
+func _ready():
+	animation_player.connect("animation_finished",_on_weapon_attack_done,CONNECT_PERSIST)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "top", "down")
@@ -20,8 +23,22 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 		animated_sprite_2d.animation = "default"
 	move_and_slide()
-
-	var mouse_position = get_global_mouse_position()
-	weapon.look_at(mouse_position)
 	if Input.is_action_just_pressed("attack"):
-		animations.play("attack")
+		var mouse_position = get_global_mouse_position()
+		var direction_to_mouse = mouse_position - global_position
+		weapon.global_position = global_position + direction_to_mouse.normalized() * 10
+		weapon.global_position.y += 2
+		weapon.visible = true
+		var angle = rad_to_deg(atan2(direction_to_mouse.y, direction_to_mouse.x))
+		if angle > -45 && angle < 45:
+			animation_player.play("attack_right")
+		else : if angle >= 45 && angle < 135:
+			animation_player.play("attack_down")
+		else : if angle >= 135 || angle < -135:
+			animation_player.play("attack_left")
+		else : if angle >= -135 && angle <= -45:
+			animation_player.play("attack_top")
+
+
+func _on_weapon_attack_done(anmiation_name):
+	weapon.visible = false
